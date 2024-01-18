613bad8b34fdf194ed9205a04cc9b186f77b996d
vlatkojuric <juricv78@gmail.com>
2024-01-18 23:31:21 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Coin from "./components/Coin";
+
+//add routes and add individual page for a coin
+//add a favorites page
+//maybe a navbar to navigate to favorite page and home page
 
 function App() {
+  const [listOfCoins, setListOfCoins] = useState([]);
+  const [searchWord, setSearchWord] = useState("");
+
+  //using the useEffect hook to display the data imediately on the page and using the dependany array to prevent it looping
+  useEffect(() => {
+    axios
+      .get("https://openapiv1.coinstats.app/coins", {
+        headers: {
+          accept: "application/json",
+          "X-API-KEY": "pVVDBw112Hhkp+h4NwfV7dQzGqe3KWPQAFW3Wpa5yzI=",
+        },
+      })
+      .then((response) => {
+        setListOfCoins(response.data.result);
+        console.log(response.data.result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const filteredCoins = listOfCoins.filter((coin) =>
+    coin.name.toLowerCase().includes(searchWord.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cryptoHeader">
+        <input
+          type="text"
+          placeholder="Bitcoin..."
+          onChange={(event) => setSearchWord(event.target.value)}
+        />
+      </div>
+      <div className="cryptoDisplay">
+        {filteredCoins.map((coin) => (
+          <Coin
+            name={coin.name}
+            icon={coin.icon}
+            price={coin.price}
+            symbol={coin.symbol}
+            key={coin.id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/Coin.js
@@ -0,0 +1,10 @@
+export default function Coin({ name, icon, symbol, price }) {
+  return (
+    <div className="coin">
+      <h1>Name : {name} </h1>
+      <img src={icon} alt="icon for each crypto" />
+      <h3>Price : {price} </h3>
+      <h3>Ticker : {symbol} </h3>
+    </div>
+  );
+}
