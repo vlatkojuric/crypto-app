793c19f8fb775d65de7ba80017ce801347d49fc0
vlatkojuric <juricv78@gmail.com>
2024-01-19 22:12:59 +0100

    added a currency to the value

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -3,7 +3,7 @@ export default function Coin({ name, icon, symbol, price }) {
     <div className="coin">
       <h1>Name : {name} </h1>
       <img src={icon} alt="icon for each crypto" />
-      <h3>Price : {price} </h3>
+      <h3>Price : {price} $ </h3>
       <h3>Ticker : {symbol} </h3>
     </div>
   );
